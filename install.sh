b4e32263a1cb34e3fec3de08abbaf335658f4f7f
Rahil Sinha <35484661+rahil-1407@users.noreply.github.com>
2021-02-14 20:41:12 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ nvm -y install node
 # sudo apt -y install npm
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+sudo apt -y update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
-sudo systemctl enable mongod
+sudo systemctl status mongod
